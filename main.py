7ddb7d455b08a93812af67278a00b55ecfcceaff
Leonardo George <leonardogeorge30@gmail.com>
2025-10-05 15:35:30 -0300

    Verificador-de-vacinação

---
+++ main.py
@@ -0,0 +1,5 @@
+peso = eval(input("Digite o peso em Kg: "))
+altura = eval(input("Digite sua altura em metros: "))
+imc = peso / (altura * altura)
+
+print("IMC= ", imc)
---
+++ vacinar-seu-filho/verificador.py
@@ -0,0 +1,19 @@
+idade = int(input("Informe a idade da criança: "))
+
+if idade < 5:
+    print("A criança deve ser vacinada")
+    print("Procure o posto de saúde mais proximo")
+    print("A Saúde vem em primeiro lugar")
+elif idade == 5:
+    print("Se a criança não foi vacinada, deve vaciná-la o mais rápido possível")
+    print("Procure o posto de saúde mais proximo")
+    print("A Saúde vem em primeiro lugar")
+else:  # idade > 5
+    # Precisamos perguntar se a criança foi vacinada
+    vacinada = input("A criança já foi vacinada? (sim/não): ").strip().lower()
+    if vacinada == 'sim':
+        print("Parabéns você vacinou seu filho")
+    else:
+        print("Vacine seu filho imediatamente")
+    print("Procure o posto de saúde mais proximo")
+    print("A Saúde vem em primeiro lugar")
